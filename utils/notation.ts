4b73d191363ac4b191f31a1a499ffa1076b8921b
Evgeny Petrunin <duen@mail.ru>
2022-12-22 13:59:42 +0300

    fix fetcher utility

--- utils/notation.ts
+++ utils/notation.ts
@@ -1,37 +1,46 @@
 interface ObjectKeys {
-    [key: string]: any;
+  [key: string]: any;
 }
 
 class Notation {
-    private transformKeys(obj: ObjectKeys, transform: (key: string) => string) {
-        const result: ObjectKeys = {};
-
-        for (let key of Object.keys(obj)) {
-            const newKey = transform(key);
-            result[newKey] = obj[key];
-        }
-
-        return result;
+  private transformKeys(obj: ObjectKeys, transform: (key: string) => string) {
+    if (typeof obj !== 'object' || obj === null) {
+      throw new Error();
     }
 
-    private transformToCamelCase = (key: string) => {
-        return key.replaceAll(/_\w/g, (str) => str.substring(1).toUpperCase());
-    };
-    private transformToSnakeCase = (key: string) => {
-        return key.replaceAll(/([A-Z])/g, '_$1').toLowerCase();
-    };
+    const result: ObjectKeys = {};
 
-    toCamelCase(obj: ObjectKeys) {
-        return this.transformKeys(obj, this.transformToCamelCase);
-    }
-    toSnakeCase(obj: ObjectKeys) {
-        return this.transformKeys(obj, this.transformToSnakeCase);
+    for (let key of Object.keys(obj)) {
+      const newKey = transform(key);
+      result[newKey] = obj[key];
     }
 
-    auto(obj: ObjectKeys) {
-        const hasUnderscore = Object.keys(obj).some((el) => el.includes('_'));
-        return hasUnderscore ? this.toCamelCase(obj) : this.toSnakeCase(obj);
-    }
+    return result;
+  }
+
+  private snakeToCamelCase = (key: string) => {
+    const transform = (string: string) => string.replaceAll(/_\w/g, (str) => str[1].toUpperCase());
+
+    const isStartsWithUnderscore = key.startsWith('_');
+
+    return isStartsWithUnderscore ? '_' + transform(key.substring(1)) : transform(key);
+  };
+
+  private camelToSnakeCase = (key: string) => {
+    return key.replaceAll(/([A-Z])/g, '_$1').toLowerCase();
+  };
+
+  toCamelCase(obj: ObjectKeys) {
+    return this.transformKeys(obj, this.snakeToCamelCase);
+  }
+  toSnakeCase(obj: ObjectKeys) {
+    return this.transformKeys(obj, this.camelToSnakeCase);
+  }
+
+  auto(obj: ObjectKeys) {
+    const hasUnderscore = Object.keys(obj).some((el) => el.includes('_'));
+    return hasUnderscore ? this.toCamelCase(obj) : this.toSnakeCase(obj);
+  }
 }
 
 export default new Notation();
